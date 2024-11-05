5870aa64f0cd98e170daf9af9ee05a0dc4f9ea7b
Brandon Smith <Bcsmith1295@gmail.com>
2024-11-04 22:45:29 -0600

    #30 old header code removed from main page now that header component exist

--- quest-app/src/app/page.tsx
+++ quest-app/src/app/page.tsx
@@ -12,20 +12,7 @@ const Home: React.FC = () => {
     <div className="min-h-screen bg-[#0A3E45] text-white font-sans">
       {/* Header */}
       <Header />
-      {/* <header className="flex justify-between items-center p-6 bg-[#123B47]">
-        <div className="flex items-center">
-          <img src="/compass-logo.png" alt="Logo" className="w-10 h-10" />
-          <span className="ml-4 bg-yellow-400 text-black py-2 px-4 rounded-full font-bold">50</span>
-        </div>
-        <nav className="flex space-x-6">
-          <a href="/" className="text-white hover:text-yellow-400">Home</a>
-          <a href="/resources" className="text-white hover:text-yellow-400">Resources</a>
-          <a href="/blog" className="text-white hover:text-yellow-400">Blog</a>
-          <a href="/contact" className="text-white hover:text-yellow-400">Contact</a>
-        </nav>
-        <img src="/metamask-logo.png" alt="MetaMask Logo" className="w-10 h-10" />
-      </header> */}
-
+      
       {/* Tabs */}
       <div className="flex justify-center space-x-4 mt-10">
         <button 

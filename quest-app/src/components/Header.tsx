08b2ca1482842be2b9978f7638db257cc583dc02
Brandon Smith <Bcsmith1295@gmail.com>
2024-11-06 20:50:20 -0600

    -30 <img> code changed to <Image /> and flex margin changed on navigation links

--- quest-app/src/components/Header.tsx
+++ quest-app/src/components/Header.tsx
@@ -2,23 +2,32 @@
 "use client"; // This directive makes the component a Client Component
 
 import React from 'react';
+import Image from 'next/image';
 
 const Header = () => {
   return (
     <header className="flex justify-between items-center p-6 bg-[#123B47]">
-      <div className="flex items-center">
-        <img src="/compass-logo.png" alt="Logo" className="w-10 h-10" />
-        <span className="ml-4 bg-yellow-400 text-black py-2 px-4 rounded-full font-bold">50</span>
+      {/* Logo and Badge */}
+      <div className="flex items-center space-x-4">
+        <Image src="/compass-logo.png" alt="Logo" width={50} height={50} />
+        <span className="bg-yellow-400 text-black py-1 px-3 rounded-full font-bold">
+          50
+        </span>
       </div>
-      <nav className="flex space-x-6">
-        <a href="/" className="text-white hover:text-yellow-400">Home</a>
-        <a href="/resources" className="text-white hover:text-yellow-400">Resources</a>
-        <a href="/blog" className="text-white hover:text-yellow-400">Blog</a>
-        <a href="/contact" className="text-white hover:text-yellow-400">Contact</a>
+
+      {/* Navigation Links */}
+      <nav className="flex">
+        <a href="/" className="text-white hover:text-yellow-400 mx-3">Home</a>
+        <a href="/resources" className="text-white hover:text-yellow-400 mx-3">Resources</a>
+        <a href="/blog" className="text-white hover:text-yellow-400 mx-3">Blog</a>
+        <a href="/contact" className="text-white hover:text-yellow-400 mx-3">Contact</a>
       </nav>
-      <img src="/metamask-logo.png" alt="MetaMask Logo" className="w-10 h-10" />
+
+      {/* Metamask Logo */}
+      <Image src="/metamask-logo.png" alt="Metamask Logo" width={50} height={50} />
     </header>
   );
 };
 
 export default Header;
+

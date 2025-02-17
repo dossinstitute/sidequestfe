cfb6f14061544b201499708f17b9812e8c8fa7ab
Brandon Smith <Bcsmith1295@gmail.com>
2025-02-17 16:59:30 -0600

    #4 updated connect wallet button to yellow

--- quest-app/src/components/Providers.tsx
+++ quest-app/src/components/Providers.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { config } from '@/lib/wagmi';
 import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { RainbowKitProvider, midnightTheme } from '@rainbow-me/rainbowkit';
 
 
 const queryClient = new QueryClient();
@@ -13,7 +13,15 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>{children}</RainbowKitProvider>
+        <RainbowKitProvider
+          theme={midnightTheme({
+            accentColor: '#FACC15',
+            accentColorForeground: 'black',
+            borderRadius: 'medium',
+          })}
+        >
+          {children}
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
